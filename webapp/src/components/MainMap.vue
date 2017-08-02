46577480da1812bb18e7a2ab3daa86f9578dfbb4
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-08-01 23:28:17 -0500

    feat: add basic google map

--- webapp/src/components/MainMap.vue
+++ webapp/src/components/MainMap.vue
@@ -3,8 +3,41 @@
 </template>
 
 <script>
+import MapsTheme from '../googleMapsApi/theme'
+
 export default {
-  name: 'MainMap'
+  name: 'MainMap',
+  mounted () {
+    this.map = new window.google.maps.Map(this.$el, this.initialState)
+    this.map.addListener('click', this.handleMapClick)
+  },
+  data () {
+    return {
+      addMode: false,
+      map: {},
+      markers: [],
+      initialState: {
+        center: {
+          lat: 25.129247,
+          lng: -49.944949
+        },
+        zoom: 3,
+        styles: MapsTheme
+      }
+    }
+  },
+  methods: {
+    handleMapClick (e) {
+      if (!this.addMode) return false
+
+      let marker = new window.google.maps.Marker({
+        position: e.latLng,
+        map: this.map
+      })
+
+      this.markers.push(marker)
+    }
+  }
 }
 </script>
 
@@ -15,5 +48,8 @@ export default {
   position: absolute
   left: 0
   width: 100%
+  .gmap
+    width: 100%
+    height: 100%
 
 </style>
