71a171efe8e80a7d65350701a16e1461b145dcd7
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-08-24 19:42:45 -0500

    feat: add production script

---
+++ production.sh
@@ -0,0 +1,15 @@
+echo "** Compiling ."
+cd webapp;
+npm run build;
+
+echo "*** Moving .."
+cd ../app_backend/frontapp;
+rm -rf *;
+cp -r ../../webapp/dist/* ./;
+
+echo "**** Uploading to Heroku ..."
+git add .
+git commit -m "feat: deploy";
+git push heroku master;
+
+echo "***** Finishing ..."
